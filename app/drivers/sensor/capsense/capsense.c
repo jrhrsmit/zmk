0abae68e798654391885e5afbbe74d2d76c97e4d
Jasper Smit <jrhrsmit@gmail.com>
2021-01-23 02:08:54 +0100

    use delayed work instead of timers

--- app/drivers/sensor/capsense/capsense.c
+++ app/drivers/sensor/capsense/capsense.c
@@ -23,30 +23,21 @@
 #define CAPSENSE_STACK_SIZE 128
 #define CAPSENSE_PRIORITY 100
 
-nrfx_timer_t capsense_nrfx_timer = NRFX_TIMER_INSTANCE(3);
 
+LOG_MODULE_REGISTER(capsense, CONFIG_SENSOR_LOG_LEVEL);
 K_THREAD_STACK_DEFINE(capsense_stack_area, CAPSENSE_STACK_SIZE);
 struct k_work_q capsense_work_q;
 
-LOG_MODULE_REGISTER(capsense, CONFIG_SENSOR_LOG_LEVEL);
-struct k_timer capsense_timer;
-
-static const struct device *sdev;
+nrfx_timer_t capsense_nrfx_timer = NRFX_TIMER_INSTANCE(3);
 
 static const struct sensor_driver_api capsense_api = {};
 
-static void capsense_nrfx_timer_callback(nrf_timer_event_t event_type, void *p_context) {
-    // LOG_DBG("Timer Int: event %d", event_type);
-}
+static void capsense_nrfx_timer_callback(nrf_timer_event_t event_type, void *p_context) {}
 
-static void capsense_nrfx_lpcomp_callback(nrf_lpcomp_event_t event_type) {
-    //LOG_DBG("COMP Int: event %d", event_type);
-    // while(true){}
-    // nrf_lpcomp_event_clear(NRF_LPCOMP, event_type);
-}
+static void capsense_nrfx_lpcomp_callback(nrf_lpcomp_event_t event_type) {}
 
 static void gpio_pullup(uint8_t pin, int bank, bool enable) {
-    if (bank) { // pin >= 32
+    if (bank) {
         if (enable)
             NRF_P1->PIN_CNF[pin] |= GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos;
         else
@@ -65,11 +56,7 @@ static int comp_init(const struct device *dev) {
     const struct capsense_config *cfg = dev->config;
     static nrf_ppi_channel_t ppi_channel;
     nrfx_err_t err;
-    /* Connect GPIOTE_0 IRQ to nrfx_gpiote_irq_handler */
-    // IRQ_CONNECT(DT_IRQN(DT_NODELABEL(gpiote)), DT_IRQ(DT_NODELABEL(gpiote), priority), nrfx_isr,
-    //            nrfx_lpcomp_irq_handler, 0);
     IRQ_CONNECT(19, 20, nrfx_isr, nrfx_lpcomp_irq_handler, 0);
-
     // comparator
     drv_data->comp_cfg = (nrfx_lpcomp_config_t)NRFX_LPCOMP_DEFAULT_CONFIG(cfg->adc_channel);
     drv_data->comp_cfg.hal.reference = NRF_LPCOMP_REF_SUPPLY_2_8;
@@ -105,13 +92,6 @@ static int comp_init(const struct device *dev) {
     return 0;
 }
 
-static void capsense_handler(struct k_timer *exp) {
-    static int i = 1;
-    struct capsense_data *drv_data = sdev->data;
-    // LOG_DBG("capsense timer interrupt %d", i++);
-    k_work_submit(&drv_data->work);
-}
-
 static uint32_t capsense_capacitance(struct device *dev) {
     // charge capacitor
     gpio_pullup(5, 0, true);
@@ -136,11 +116,12 @@ static uint32_t capsense_capacitance(struct device *dev) {
 #define SAMPLE_SZ 64
 #define THRESHOLD 10
 static void capsense_work(struct k_work *item) {
-    // const struct capsense_config *cfg = sdev->config;
     static uint32_t val[BUF_SZ] = {0};
     static uint32_t sample[SAMPLE_SZ] = {0};
     static uint32_t idx = 0;
     uint32_t low = 0xFFFFFFFF, high = 0, sum = 0, avg, sample_avg = 0;
+    struct capsense_data *const drv_data = CONTAINER_OF(item, struct capsense_data, work);
+    struct device *const dev = drv_data->device_struct;
     // maybe use IIR filter?
     for (int i = 0; i < BUF_SZ; i++) {
         if (val[i] < low)
@@ -150,23 +131,23 @@ static void capsense_work(struct k_work *item) {
         sum += val[i];
     }
     avg = sum / BUF_SZ;
-    for(int i=0; i<SAMPLE_SZ; i++) {
-        sample[i] = capsense_capacitance(sdev);
+    for (int i = 0; i < SAMPLE_SZ; i++) {
+        sample[i] = capsense_capacitance(dev);
     }
-    for(int i=SAMPLE_SZ-1; i>0; i--) {
-        for(int j=0; j<i; j++) {
-            if(sample[j] > sample[j+1]){
-                uint32_t tmp = sample[j+1];
-                sample[j+1] = sample[j];
+    for (int i = SAMPLE_SZ - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (sample[j] > sample[j + 1]) {
+                uint32_t tmp = sample[j + 1];
+                sample[j + 1] = sample[j];
                 sample[j] = tmp;
             }
         }
     }
     // calculate average excluding top and bottom 10% of samples
-    for(int i=SAMPLE_SZ/10; i<SAMPLE_SZ*9/10; i++) {
+    for (int i = SAMPLE_SZ / 10; i < SAMPLE_SZ * 9 / 10; i++) {
         sample_avg += sample[i];
     }
-    sample_avg = sample_avg / ((SAMPLE_SZ*9/10) - (SAMPLE_SZ/10));
+    sample_avg = sample_avg / ((SAMPLE_SZ * 9 / 10) - (SAMPLE_SZ / 10));
     val[idx] = sample_avg;
     if (val[idx] > avg + THRESHOLD)
         LOG_DBG("Presence detected (%d (avg: %d))", val[idx], avg);
@@ -174,12 +155,13 @@ static void capsense_work(struct k_work *item) {
         LOG_DBG("(%d (avg: %d))", val[idx], avg);
     if (++idx > BUF_SZ - 1)
         idx = 0;
+    k_delayed_work_submit(&drv_data->work, K_MSEC(250));
 }
 
 static int capsense_init(const struct device *dev) {
     struct capsense_data *drv_data = dev->data;
     const struct capsense_config *cfg = dev->config;
-    sdev = dev;
+    drv_data->device_struct = dev;
     // comp
     if (comp_init(dev)) {
         LOG_ERR("Could not initialize COMP");
@@ -188,11 +170,9 @@ static int capsense_init(const struct device *dev) {
     // work queue
     k_work_q_start(&capsense_work_q, capsense_stack_area,
                    K_THREAD_STACK_SIZEOF(capsense_stack_area), CAPSENSE_PRIORITY);
-    k_work_init(&drv_data->work, capsense_work);
-    // timers
-    k_timer_init(&capsense_timer, capsense_handler, NULL);
-    k_timer_start(&capsense_timer, K_SECONDS(3), K_MSEC(250));
-
+    k_delayed_work_init(&drv_data->work, capsense_work);
+    k_delayed_work_submit(&drv_data->work, K_MSEC(250));
+    
     LOG_DBG("Capsense initialized");
     LOG_DBG("GPIO pin: %d, ADC pin: %d", cfg->pin, cfg->adc_channel);
     return 0;
